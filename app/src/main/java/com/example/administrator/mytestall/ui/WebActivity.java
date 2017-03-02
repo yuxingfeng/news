c2744a7fa884357f1fe8851b4bc90960fac802eb
feng <1652026179@qq.com>
2017-03-02 16:17:05 +0800

    新闻客户端
    本次更新动画

--- app/src/main/java/com/example/administrator/mytestall/ui/WebActivity.java
+++ app/src/main/java/com/example/administrator/mytestall/ui/WebActivity.java
@@ -1,5 +1,6 @@
 package com.example.administrator.mytestall.ui;
 
+import android.animation.Animator;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,18 +9,25 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.Window;
+import android.view.animation.AccelerateDecelerateInterpolator;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
 
 import com.example.administrator.mytestall.R;
 import com.example.administrator.mytestall.view.ProgressWebView;
+
+import java.lang.ref.WeakReference;
 
 
 public class WebActivity extends AppCompatActivity {
     protected ProgressWebView event_wv_context;
     Toolbar toolbar;
+    LinearLayout mRootLayout;
+    MyRunnable myRunnable;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
@@ -29,11 +37,29 @@ public class WebActivity extends AppCompatActivity {
 
         initView();
         initData();
-    }
 
+    }
+    static class MyRunnable implements Runnable{
+        private WeakReference<WebActivity> weakActivity;
+        public MyRunnable(WebActivity activity) {
+            weakActivity = new WeakReference<WebActivity>(activity);
+        }
+
+        @Override
+        public void run() {
+            WebActivity activity = weakActivity.get();
+            if (null != activity) {
+                activity.mRootLayout.setVisibility(View.VISIBLE);
+                Animator animator = activity.createRevealAnimator(false, 0, 0);
+                animator.start();
+            }
+        }
+    }
     private void initView() {
+        myRunnable=new MyRunnable(this);
+        mRootLayout= (LinearLayout) findViewById(R.id.mRootLayout);
         toolbar= (Toolbar) findViewById(R.id.toolbar_web);
-        toolbar.setNavigationIcon(R.mipmap.ic_launcher);//设置导航栏图标
+        toolbar.setNavigationIcon(R.mipmap.ahc);//设置导航栏图标
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,6 +68,10 @@ public class WebActivity extends AppCompatActivity {
         });
         toolbar.setTitle("正在加载...");
         event_wv_context = (ProgressWebView) findViewById(R.id.wb);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(view->onBackPressed());
+        mRootLayout.post(myRunnable);
     }
 
     private void initData() {
@@ -76,5 +106,48 @@ public class WebActivity extends AppCompatActivity {
             }
         });
 
+    }
+    /**
+     * 揭露动画
+     */
+    private Animator createRevealAnimator(boolean exit, int x, int y) {
+        float hypot = (float) Math.hypot(mRootLayout.getHeight(),mRootLayout.getWidth());
+        float startRadius = exit ? hypot : 0;
+        float endRadius = exit ? 0 : hypot;
+
+        Animator animator = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            animator = ViewAnimationUtils.createCircularReveal(mRootLayout,x,y,startRadius,endRadius);
+        }
+        animator.setDuration(800);
+        animator.setInterpolator(new AccelerateDecelerateInterpolator());
+        if (exit){
+            animator.addListener(animatorListenerExit);
+        }
+        return animator;
+    }
+    private Animator.AnimatorListener animatorListenerExit = new Animator.AnimatorListener() {
+        @Override
+        public void onAnimationStart(Animator animation) {
+        }
+
+        @Override
+        public void onAnimationEnd(Animator animation) {
+            //动画结束时，销毁当前Activity
+            mRootLayout.setVisibility(View.INVISIBLE);//在finish()的时候会闪屏的现象，先不可见，再销毁就不会闪屏了
+            finish();
+        }
+
+        @Override
+        public void onAnimationCancel(Animator animation) {
+        }
+
+        @Override
+        public void onAnimationRepeat(Animator animation) {
+        }
+    };
+    public void onBackPressed() {
+        Animator animator = createRevealAnimator(true, mRootLayout.getWidth()/2, mRootLayout.getHeight()/2);
+        animator.start();
     }
 }
